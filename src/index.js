cb04a43424135cf8824d257e645fe8ea12eac620
daria marczak <marczak.ria@gmail.com>
2018-10-26 18:15:43 +0200

    Setup for react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = () => {
+  return <div>Hello React!</div>;
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
